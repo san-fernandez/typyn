2781ea7e80666d8aed08558a396e1421b6027cdc
sannfernandez <santifernandezok1+personal@gmail.com>
2024-03-16 16:36:22 -0300

    tried inquirer for selection and join to print

--- main.py
+++ main.py
@@ -5,16 +5,29 @@ import inquirer
 
 app = typer.Typer()
 
-@app.command()
-def hello(name: str):
-    print(f"Hello {name}")
+def select_random_words(path, count):
+
+    with open(path, "r", encoding="utf-8") as file:
+        word_list = file.read().splitlines()
+
+    random_words = random.sample(word_list, count)
+    return random_words
+
 
 @app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        print(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        print(f"Bye {name}!")
+def welcome():
+    
+    questions = [inquirer.List('language', message="choose language:", choices=["english", "español"]),
+    inquirer.Text("word_count", message="Enter number of words", validate=lambda _, x: int(x) > 0),]
+
+    answers = inquirer.prompt(questions)
+
+    language = answers["language"]
+
+    word_count = int(answers["word_count"])
+    words = select_random_words(f"words/{language[0:2].lower()}-1000", word_count)
+
+    typer.echo(" ".join(words))
 
 if __name__ == "__main__":
     app()
